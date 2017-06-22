571ae19dd35b332c44681cbb33ccc4e88257580d
Filip Schouwenaars <filip@datacamp.com>
2017-06-22 10:50:23 +0200

    last version, maybe?

--- requirements.r
+++ requirements.r
@@ -1,5 +1,5 @@
 devtools::install_version("dplyr", "0.5.0")
-devtools::install_github("joshuaulrich/quantmod@157_yahoo_502")
+devtools::install_github("joshuaulrich/quantmod")
 devtools::install_version("zoo", "1.7-14")
 devtools::install_version("xts", "0.9-7")
 devtools::install_version("TTR", "0.23-1")
